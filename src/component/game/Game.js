75891e3931d0a8d12bd8f2b79c22dd3d68c6ef89
sibhat <sibhat.temelso@gmail.com>
2018-09-27 13:02:38 -0700

    game map has been added

--- src/component/game/Game.js
+++ src/component/game/Game.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import Pusher from "pusher-js";
 import Spiner from "../layout/Spiner";
 import "./game.css";
+import ReactTooltip from "react-tooltip";
 
 class Game extends Component {
 	constructor(props) {
@@ -17,7 +18,9 @@ class Game extends Component {
 			error_msg: null,
 			message: [],
 			dispatch: true,
-			messageIsOpen: false
+			messageIsOpen: false,
+			rooms: [],
+			mapIsOpen: false
 		};
 	}
 	componentDidMount() {
@@ -53,6 +56,15 @@ class Game extends Component {
 				console.log({ error });
 				this.setState({ dispatch: false });
 			});
+		axios
+			.get(`${url}/api/adv/rooms`)
+			.then(response => {
+				console.log(response.data);
+				this.setState({ rooms: response.data.rooms });
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	}
 	handleInput = e => {
 		let comand = e.target.value;
@@ -65,8 +77,23 @@ class Game extends Component {
 				data = { direction: comand };
 			} else if (comand.split(" ")[0].toLowerCase() === "say") {
 				url += "/api/adv/say/";
-				comand = comand.split(" ")[1];
+				comand = comand.split(" ");
+				comand.shift();
+				comand = comand.join(" ");
 				data = { message: comand };
+				console.log(comand);
+			} else if (comand.split(" ")[0].toLowerCase() === "shout") {
+				url += "/api/adv/shout/";
+				comand = comand.split(" ");
+				comand.shift();
+				comand = comand.join(" ");
+				data = { message: comand };
+			} else if (comand.split(" ")[0].toLowerCase() === "whisper") {
+				url += "/api/adv/whisper/";
+				comand = comand.split(" ");
+				comand.shift();
+				comand = comand.join(" ");
+				data = { message: comand, to: "testuser3" };
 			}
 			axios
 				.post(`${url}`, data, {
@@ -89,6 +116,9 @@ class Game extends Component {
 	handleMessages = e => {
 		this.setState({ messageIsOpen: !this.state.messageIsOpen });
 	};
+	handleMessages2 = e => {
+		this.setState({ mapIsOpen: !this.state.mapIsOpen });
+	};
 	render() {
 		let {
 			name,
@@ -101,10 +131,41 @@ class Game extends Component {
 			dispatch
 		} = this.state;
 		let messages = message.map((m, i) => <p key={i}>{m.message}</p>);
+		let pos = [[72, 10], [40, 13], [10, 10], [40, 55], [20, 70]];
+		let rooms = this.state.rooms.map((room, i) => {
+			return (
+				<div
+					className="landing__room"
+					style={{ top: `${pos[i][0]}%`, left: `${pos[i][1]}%` }}
+					key={i}
+					data-tip={
+						room.length > 2
+							? `${room[1]}  players: ${room[2]}`
+							: room[1]
+					}
+				>
+					{room[0]}
+					<ReactTooltip />
+				</div>
+			);
+		});
 		return dispatch ? (
 			<Spiner />
 		) : (
 			<div className="game">
+				<button
+					className="btn game__messages--btn"
+					onClick={this.handleMessages2}
+				>
+					{this.state.mapIsOpen && message.length > 0
+						? "close"
+						: "open"}
+				</button>
+
+				{this.state.mapIsOpen ? (
+					<span className="game__map"> {rooms} </span>
+				) : null}
+
 				<div className="game__center">
 					<div className="game_header">
 						<strong>{name}</strong> : id [ {uuid} ]
--- src/component/game/Landing.js
+++ src/component/game/Landing.js
@@ -1,14 +1,47 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import ReactTooltip from "react-tooltip";
+
 import "./game.css";
 class Landing extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
+		this.state = {
+			rooms: []
+		};
+	}
+	componentDidMount() {
+		let url = "https://sibhat-lambdamud.herokuapp.com";
+		url = "http://127.0.0.1:8800";
+		axios
+			.get(`${url}/api/adv/rooms`)
+			.then(response => {
+				console.log(response.data);
+				this.setState({ ...response.data });
+			})
+			.catch(error => {
+				console.log(error);
+			});
 	}
 	render() {
+		let pos = [[72, 10], [40, 13], [10, 10], [40, 55], [20, 70]];
+		let rooms = this.state.rooms.map((room, i) => {
+			return (
+				<div
+					className="landing__room"
+					style={{ top: `${pos[i][0]}%`, left: `${pos[i][1]}%` }}
+					key={i}
+					data-tip={room[1]}
+				>
+					{room[0]}
+					<ReactTooltip />
+				</div>
+			);
+		});
 		return (
 			<div className="landing">
+				{rooms}
 				<div className="landing__textBox">
 					<Link to="/auth/register" className="landing__link">
 						Create Account
